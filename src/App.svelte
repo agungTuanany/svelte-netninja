f38636a10d78aee74897a4c959b8f8867b4f85b2
agungTuanany <agung.tuanany@gmail.com>
2021-03-05 05:59:12 +0700

    feature(5-conditional): conditional in svelte

--- src/App.svelte
+++ src/App.svelte
@@ -3,24 +3,48 @@
             { name: "bruono", beltcolor: "black", age:25, id: 1 },
             { name: "mario", beltcolor: "orange", age:45, id: 2 },
             { name: "lugi", beltcolor: "brown", age:35, id: 3 },
-            { name: "kingpin", beltcolor: "blue", age:25, id: 4 }
+            { name: "kingpin", beltcolor: "blue", age:25, id: 4 },
+            { name: "liam", beltcolor: "", age:25, id: 5 }
         ];
 
-    // 'e' is the 'event-object' we pass on the 'inline-event-handler' to know what button was fired up
-    // you can unassigned the `event-object' as well.
-    // in here is just an example to passing event object on 'inline-event-handler'.
     const handleClick = (e, id) => {
             // delete the person from people
             people = people.filter((person) => person.id != id);
             console.log(e);
         };
+
+    let num = 2;
 </script>
 
+
+{#if num >  20}
+    <p>{num} greater than 20 </p>
+{:else if num > 5}
+    <p>{num} greater than 5</p>
+{:else if num === 5}
+    <p>{num} is equal 5</p>
+{:else if num < 5}
+    <p>{num} less than 5</p>
+{:else}
+    <p>please input some number</p>
+{/if}
+<input type="text" bind:value={num}/>
 
 <main>
     {#each people as person (person.id)}
         <div>
             <h4>{person.name}</h4>
+            {#if person.beltcolor === "black"}
+                <strong>Master Ninja</strong>
+            {:else if person.beltcolor === "orange"}
+                <strong>Tanker Ninja</strong>
+            {:else if person.beltcolor === "blue"}
+                <strong>Warrior Ninja</strong>
+            {:else if person.beltcolor === "brown"}
+                <strong>Solid Ninja</strong>
+            {:else}
+                <strong>'{person.name}' don't have any belt yet</strong>
+            {/if}
             <p>{person.age} years old, {person.beltcolor} belt.</p>
 
             <!-- XXX pass the data with inline function  XXX -->
