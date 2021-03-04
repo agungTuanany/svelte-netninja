76e2e5817e10e6b3a20e5f0187b8fbeeb3a3cedd
agungTuanany <agung.tuanany@gmail.com>
2021-03-04 19:55:37 +0700

    feature(reactive-value): reactive values

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,17 @@
 <script>
     let inputName = "black";
+    let firstName = "Aroon";
+    let lastName = "Swatch";
+
+    // reactive-value
+    $: fullName = `${firstName} ${lastName}`
+
+    // reactive-statement
+    $: {
+            console.log(inputName);
+            // You can use reactive-value in reactive-statement
+            console.log(fullName);
+        }
 
     const handleClick = () => {
             inputName = "orange";
@@ -12,38 +24,34 @@
 
 
 <main>
-    <h1 style="color:{inputName}" >Hello {inputName}!</h1>
-    <p style="color:{inputName}">{inputName}</p>
+    <h1>Hello {fullName} - {inputName}!</h1>
+    <p>{inputName}</p>
     <button on:click={handleClick}>Update Color</button>
 
-    <!-- XXX one-way data-binding XXX -->
-    <!-- <input type="text" on:input={handleInput} /> -->
+    <input type="text" bind:value={firstName}>
+    <input type="text" bind:value={lastName}>
+    <input type="text" bind:value={inputName}>
 
-    <!-- XXX two-way data-binding XXX -->
-    <!-- <input type="text" on:input={handleInput} value=inputName} /> -->
-
-    <!-- OR -->
-    <input type="text" bind:value={inputName} />
 </main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+    main {
+        text-align: center;
+        padding: 1em;
+        max-width: 240px;
+        margin: 0 auto;
+    }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+    h1 {
+        color: #ff3e00;
+        text-transform: capitalize;
+        font-size: 4em;
+        font-weight: 100;
+    }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    @media (min-width: 640px) {
+        main {
+            max-width: none;
+        }
+    }
 </style>
