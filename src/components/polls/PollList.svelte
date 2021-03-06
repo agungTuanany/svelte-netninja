0008aebeb8f3f17e7119d421d6c0f387c767e082
agungTuanany <agung.tuanany@gmail.com>
2021-03-06 17:37:47 +0700

    feature(ninjapolls): svelte lifecycle-hooks

--- src/components/polls/PollList.svelte
+++ src/components/polls/PollList.svelte
@@ -1,18 +1,49 @@
 <script>
+    ///import { onMount, onDestroy } from "svelte";
+
     import PollStore from "../../stores/PollStore.js"
-
     import  PollDetails from "./PollDetails.svelte";
-    export let polls = [];
 
 
-    PollStore.subscribe((data) => {
-        polls = data;
-    });
+    /* {{{
+     * It is always a good idea to unsubscribe from a Store when you no longer
+     * need it; when the component destroyed.
+     *
+     * Every time we want to 'subscribe' to a 'Store' and 'unsubscribe' to
+     * a 'Store' we call 'subscribe()' and store it into a variable; and then we
+     * call it in 'onDestroy' lifecycle-hooks. There's an easy way to do this.
+     *
+     * An easy way to subscribe to the data and automatically unsubscribe to the
+     * data when the component is destroyed is just to refer to the 'PollStore'
+     * instead of 'let polls = []'.
+     *
+     * We put `$` in front of `PollStore' name that we imported; it's assumes
+     * then that we want to get the 'data' from that Store, which is the list of
+     * the array of polls.
+     *
+     * So '$PollStore' is now the 'array of polls'. We cycle through as before
+     * and refer to each one as a 'poll'.
+     *
+     * So, with '$PollStore', it's automatically subscribe us to that data.
+    }}}*/
+
+    // export let polls = [];
+
+    // const unsub = PollStore.subscribe((data) => {
+    //     polls = data;
+    // });
+
+
+    // onDestroy(() => {
+    //     // unsub from store
+    //     unsub;
+    // });
+
 </script>
 
 
 <div class="poll-list">
-    {#each polls as poll (poll.id)}
+    {#each $PollStore as poll (poll.id)}
         <div>
             <PollDetails {poll} on:vote />
         </div>
